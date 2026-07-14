7eaaf3a82413cbbcf7c42d82d8b2463c8e0c0e04
= <=>
2026-07-14 17:36:55 +0300

    replace ))

--- app/components/SiteAudio.tsx
+++ app/components/SiteAudio.tsx
@@ -65,7 +65,7 @@ export default function SiteAudio() {
         }`}
       />
       <span className="text-xl leading-none sm:text-2xl" aria-hidden="true">
-        {")))"}
+        {"♪₊˚⊘"}
       </span>
     </button>
   );
